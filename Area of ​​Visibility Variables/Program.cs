203307c1cfca92c3d0045c5e0e8207188979fe07
Denis Sidorov <dsigmat@hotmail.com>
2019-11-25 12:06:19 +0600

    literals, avv

---
+++ Area of ​​Visibility Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Area_of___Visibility_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            {
+                var b = 20;
+                {
+                    var c = 30;
+                    Console.WriteLine($"{a}, {b} , {c}");
+                }
+                //Console.WriteLine($"{a}, {b} , {c}");
+            }
+            //Console.WriteLine($"{a}, {b} , {c}");
+        }
+    }
+}
--- Literals/Program.cs
+++ Literals/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace from_beginner_to_specialist
+namespace Literals
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int x = 0b11;
+            int y = 0b1001;
+            int k = 0b10001001;
+            int m = 0b1000_1001;
+            Console.WriteLine($"x = {x}, y = {y}, k = {k}, m = {m}");
 
+            Console.WriteLine();
+
+            x = 0x1f;
+            y = 0xff0d;
+            k = 0x1fab30ef;
+            m = 0x1fab_30ef;
+            Console.WriteLine($"x = {x}, y = {y}, k = {k}, m = {m}");
+        }
+
+        static void Variables()
+        {
             int x = 10;
             int y;
 
@@ -33,8 +49,6 @@ namespace from_beginner_to_specialist
 
             object obj = 1;
             object obj2 = "text";
-
-            Console.WriteLine($"{obj2}, {f}, {str}");
         }
     }
 }
