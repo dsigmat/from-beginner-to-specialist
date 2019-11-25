8c7cb6daa43dfb16b6876dfcdb0162cae0014db3
Denis Sidorov <dsigmat@hotmail.com>
2019-11-25 10:40:50 +0600

    Add project files.

---
+++ from beginner to specialist/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace from_beginner_to_specialist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y;
+            y = 2;
+            Int32 x1 = -1;
+            uint z = 1;
+            float f = 1.1f;
+            double d = 2.3;
+
+            int x2 = 0;
+            var a = 4;
+            var u = 5.2;
+
+
+
+
+
+
+        }
+    }
+}
