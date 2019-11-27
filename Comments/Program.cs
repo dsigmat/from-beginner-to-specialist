b5f479c50f594127d27fd22ec65e4e07380d8864
Denis Sidorov <dsigmat@hotmail.com>
2019-11-27 16:30:15 +0600

    Comments

---
+++ Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Privedeniye tipov i parsing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Privedeniye_tipov_i_parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 3;//0000 0011
+            int i = b;//0000 0000 0000 0000 0000 0000 0000 0011
+            long l = i;//0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0011
+
+            Console.WriteLine($"b = {b}, i = {i}, l = {l}");
+
+            float f = i;//3.0
+            Console.WriteLine(f);
+
+            b = (byte)i;
+            Console.WriteLine(b);
+
+            i = (int)f;
+            Console.WriteLine(i);
+
+            f = 2.1f;
+            i = (int)f;
+            Console.WriteLine(i);
+
+            string str = "1";
+            i = int.Parse(str);
+            Console.WriteLine(i);
+
+            l = long.Parse(str);
+            Console.WriteLine(l);
+
+            int x = 5;
+            int y = 2;
+            int result = x / y;
+
+            float result2 = (float)x / y;
+            Console.WriteLine(result2);
+
+
+
+        }
+    }
+}
