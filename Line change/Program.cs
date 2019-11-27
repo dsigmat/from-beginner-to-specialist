7a8286c0259c2646c1445897c866ef1fc4f9b931
Denis Sidorov <dsigmat@hotmail.com>
2019-11-27 09:57:12 +0600

    String Builder

--- Line change/Program.cs
+++ Line change/Program.cs
@@ -6,7 +6,42 @@ namespace Line_change
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = string.Empty;//пустая стррока
+
+            string nameConcat = string.Concat("My"," name"," is"," John");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = "My " + "name " + "is " + "John";
+            Console.WriteLine(nameConcat);
+
+            nameConcat = string.Join(" ","My","name","is", "John");
+            Console.WriteLine(nameConcat);
+                       
+            string newName = nameConcat.Insert(0, "By the way, ");
+            Console.WriteLine(newName);
+
+            newName = nameConcat.Remove(0, 5);
+            Console.WriteLine(newName);
+
+            string replaced = nameConcat.Replace('n','z');
+            Console.WriteLine(replaced);
+
+            string data = "12;25;64";
+            string[] splitData = data.Split(';');
+            string first = splitData[1];
+            Console.WriteLine(first);
+
+            char[] chars = nameConcat.ToCharArray();
+            Console.WriteLine(chars[0]);
+            Console.WriteLine(nameConcat[0]);
+
+            string lower = nameConcat.ToLower();
+            Console.WriteLine(lower);
+            string upper = nameConcat.ToUpper();
+            Console.WriteLine(upper);
+
+            string john = " My name is John ";
+            Console.WriteLine(john.Trim());
         }
     }
 }
---
+++ String Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My ");
+            sb.Append("name ");
+            sb.Append("is ");
+            sb.Append("John");
+            sb.AppendLine("!");
+            sb.AppendLine("Hello!");
+            //Console.WriteLine(sb);
+
+            string str = sb.ToString();
+            Console.WriteLine(str);
+        }
+    }
+}
