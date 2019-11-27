540aa003e9ef850cacf5605757c351337ec92393
Denis Sidorov <dsigmat@hotmail.com>
2019-11-27 12:20:42 +0600

    We work with the console

--- String comparison/Program.cs
+++ String comparison/Program.cs
@@ -6,7 +6,26 @@ namespace String_comparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string txt1 = "abcde";
+            string txt2 = "abcde";
+
+            bool areEqual = txt1 == txt2;//тоже самое
+            Console.WriteLine(areEqual);
+
+            areEqual = string.Equals(txt1, txt2, StringComparison.Ordinal);//тоже самое
+            Console.WriteLine(areEqual);
+
+            Console.WriteLine();
+
+            string txt3 = "Strasse";
+            string txt4 = "Straße";
+
+            areEqual = string.Equals(txt3, txt4, StringComparison.Ordinal);//тоже самое
+            Console.WriteLine(areEqual);
+            areEqual = string.Equals(txt3, txt4, StringComparison.InvariantCulture);
+            Console.WriteLine(areEqual);
+            areEqual = string.Equals(txt3, txt4, StringComparison.CurrentCulture);
+            Console.WriteLine(areEqual);
         }
     }
 }
---
+++ We work with the console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace We_work_with_the_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.WriteLine("Hi, please tell me your name");
+            string name = Console.ReadLine();
+            string sentence = $"Your name is {name}";
+            Console.WriteLine(sentence);
+
+            Console.WriteLine("Hi, please tell me your age");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);//приводим к числу
+            sentence = $"Your age is {age}";
+            Console.WriteLine(sentence);
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.Write("New Style ");
+            Console.Write("New Style\n");
+        }
+    }
+}
