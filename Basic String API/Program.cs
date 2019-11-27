095d242141c165d192817bc3614788190e188808
Denis Sidorov <dsigmat@hotmail.com>
2019-11-27 09:06:08 +0600

    Line change

--- Basic String API/Program.cs
+++ Basic String API/Program.cs
@@ -6,7 +6,32 @@ namespace Basic_String_API
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "abrsakadabra";
+            bool containtsA = name.Contains('d');
+            bool containtsE = name.Contains('E');
+
+            Console.WriteLine(containtsA);
+            Console.WriteLine(containtsE);
+
+            bool endsWithAbra = name.EndsWith("abra");
+            Console.WriteLine(endsWithAbra);
+
+            bool startWithAbra = name.StartsWith("abrs");
+            Console.WriteLine(startWithAbra);
+
+            int indxOfA = name.IndexOf('a', 1);            
+            Console.WriteLine(indxOfA);
+
+            int lastIndxOfR = name.LastIndexOf('r');
+            Console.WriteLine(lastIndxOfR);
+
+            Console.WriteLine(name.Length);
+
+            string substringFrom5 = name.Substring(5);
+            string substringFromTo = name.Substring(0,3);
+            Console.WriteLine(substringFrom5);
+            Console.WriteLine(substringFromTo);
         }
     }
 }
+ 
---
+++ Empty line/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Empty_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string empty = "";
+            string whiteSpaced = " ";
+            string notEmpty = " b";
+            string nullString = null;
+
+            Console.WriteLine("IsNullOrEmpty");
+
+            bool IsNullOrEmpty = string.IsNullOrEmpty(nullString);
+            Console.WriteLine(IsNullOrEmpty);
+
+            IsNullOrEmpty = string.IsNullOrEmpty(whiteSpaced);
+            Console.WriteLine(IsNullOrEmpty);
+
+            IsNullOrEmpty = string.IsNullOrEmpty(notEmpty);
+            Console.WriteLine(IsNullOrEmpty);
+
+            IsNullOrEmpty = string.IsNullOrEmpty(empty);
+            Console.WriteLine(IsNullOrEmpty);
+
+            Console.WriteLine("isNullOrWhiteSpace");
+            
+            bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(nullString);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(whiteSpaced);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(notEmpty);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(empty);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+        }
+    }
+}
