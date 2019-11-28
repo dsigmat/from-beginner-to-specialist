f81ab69135abcaa5236d66d46eb1471934b30367
Denis Sidorov <dsigmat@hotmail.com>
2019-11-28 12:20:12 +0600

    Homework_1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Ваше имя пжлста!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите целое число!");
+            string input = Console.ReadLine();
+            int x = int.Parse(input);
+
+            Console.WriteLine("Введите еще одно целое число!");
+            input = Console.ReadLine();
+            int y = int.Parse(input);
+
+            Console.WriteLine($"Вы ввели вот эти два числа - {x} и {y}");
+
+            Console.WriteLine();
+
+            int c = x;
+            x = y;
+            y = c;
+            Console.WriteLine($"Воть так {x},{y}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите целое число!");
+            int num = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Вот то что вы ввели {num}, и оно состоит из {num.ToString().Length} цифр.");
+
+        }
+    }
+}
--- Introducing DateTime/Program.cs
+++ Introducing DateTime/Program.cs
@@ -6,7 +6,21 @@ namespace Introducing_DateTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine($"it's {now.Date}, {now.Hour}:{now.Minute}.");
+
+            DateTime dt = new DateTime(2016,2,28);
+            DateTime newDt = dt.AddHours(1);
+            Console.WriteLine(dt);
+            Console.WriteLine(newDt);
+
+            TimeSpan ts = now - dt;
+            //ts = now.Subtract(dt);
+            Console.WriteLine(ts.Days);
+
         }
     }
 }
