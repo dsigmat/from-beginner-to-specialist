1be998640ef7232b1d4f6840960efd4541650c33
Denis Sidorov <dsigmat@hotmail.com>
2019-11-28 14:46:05 +0600

    Homework_2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины трех сторон треугольника.");
+            Console.WriteLine("x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("y:");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("z:");
+            double z = double.Parse(Console.ReadLine());
+
+            double p = (x + y + z) / 2;
+            double result = (p * (p - x)*(p - y)*(p - z));
+            double st = Math.Sqrt(result);
+
+            Console.WriteLine($"Площадь треугольника равна {st}");
+        }
+    }
+}
