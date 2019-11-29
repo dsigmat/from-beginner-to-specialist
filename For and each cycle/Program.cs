15f2e8e9c0d54759821ffa8eac7bd92ea400aa7e
Denis Sidorov <dsigmat@hotmail.com>
2019-11-29 16:03:42 +0600

    For and each cycle

---
+++ For and each cycle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For_and_each_cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Homework_4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сейчас вы введете два целочисленных значения а я найду максимальное значение.");
+            Console.WriteLine("Введите первое целочисленнроое значение!");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе целочисленнроое значение!");
+            int b = int.Parse(Console.ReadLine());
+
+            //1-st
+            //if (a > b)
+            //{
+            //    Console.WriteLine($"Число {a} больше {b}");
+            //}
+            //else if (a < b)
+            //{
+            //    Console.WriteLine($"Число {b} больше {a}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число {b} равно {a}");
+            //}
+
+            //2nd
+            //int max = a;
+
+            //if (b > a)
+            //{
+            //    max = b;
+            //}
+            //Console.WriteLine($"Max = {max}");
+
+            //int max;
+            //if (b > a) 
+            //{
+            //    max = b;
+            //}
+            //else
+            //{
+            //    max = a;
+            //}
+            //Console.WriteLine($"Max = {max}");
+
+            //3rd
+            int max = a > b ? a : b;
+
+            Console.WriteLine($"Max = {max}");
+        }
+    }
+}
