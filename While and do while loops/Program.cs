11672ee50d3663acac35f50ea11f6af38b0361fb
Denis Sidorov <dsigmat@hotmail.com>
2019-12-03 12:19:06 +0600

    Break and continue loop control

--- While and do while loops/Program.cs
+++ While and do while loops/Program.cs
@@ -6,7 +6,32 @@ namespace While_and_do_while_loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 30;
+
+            while (age < 18)
+            {
+                Console.WriteLine("First while loop");
+                Console.WriteLine("What is your age?");
+                age = int.Parse(Console.ReadLine());
+            }
+
+            do
+            {
+                Console.WriteLine("Do\\While");
+                Console.WriteLine("What is your age?");
+                age = int.Parse(Console.ReadLine());
+            } while (age < 18);
+
+            Console.WriteLine("Good");
+
+            int[] numbers = { 1,2,5,8,9,6,7,8,4 };
+
+            int i = 0;
+            while (i < numbers.Length)
+            {
+                Console.Write(numbers[i] + ", ");
+                i++;
+            }
         }
     }
 }
---
+++ break and continue loop control/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace break_and_continue_loop_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
