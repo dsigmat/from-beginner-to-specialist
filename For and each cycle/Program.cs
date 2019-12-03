b4411d62fa44fa9f2c1c0ef3c45ab2360683963f
Denis Sidorov <dsigmat@hotmail.com>
2019-12-03 09:56:59 +0600

    Vlozhennyĭ tsikl for

--- For and each cycle/Program.cs
+++ For and each cycle/Program.cs
@@ -6,7 +6,36 @@ namespace For_and_each_cycle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 1, 2, 6, 2, 1, 2, 5, 8, 120 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]%2 == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Vlozhennyĭ tsikl for/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vlozhennyĭ_tsikl_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
