4f64a425bde2e7516135278f2c0fb0cf72d502b9
Denis Sidorov <dsigmat@hotmail.com>
2019-12-04 11:56:59 +0600

    повторение мать учение

--- break and continue loop control/Program.cs
+++ break and continue loop control/Program.cs
@@ -6,7 +6,69 @@ namespace break_and_continue_loop_control
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numb = { 0, 1, 2, 5, 4, 3, 6, 7, 8, 9 };
+
+            foreach (var item in numb)
+            {
+                //if (item % 2 != 0)
+                //{
+                //    Console.Write(  item + " " );
+                //}
+
+                if (item % 2 == 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+
+
+
+            char[] letters = { 'a', 'b','c','d','f','j', 'h', 'x', 'z','o' };
+
+            for (int i = 0; i < numb.Length; i++)
+            {
+                Console.WriteLine($"Number = {numb[i]}");
+                for (int x = 0; x < letters.Length; x++)
+                {
+                    if (numb[i] == x)
+                        break;
+                    Console.Write($"{letters[x]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+            int[] numbers = { 1,-2,4,-7,5,3,2,-1,-3,2,7,-1,-3,1,7};
+
+            int counter = 0;            
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (counter == 3)
+                {
+                    break;
+                }
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair ({atI};{atJ}). Indexes ({i};{j})");
+                        counter++;
+                    }
+
+                    if (counter == 3)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{i}");
+            }
         }
     }
 }
---
+++ if_else/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Сколько лет?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько ты весишь, кожаный ублюдок!");
+            double weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Твой рост!");
+            double height = double.Parse(Console.ReadLine());
+
+            double bodyMassIndex = weight / (height * height);
+
+            bool isTooLow = bodyMassIndex <= 18.5;// недостаток веса
+            bool isNormal = bodyMassIndex > 18.5 && bodyMassIndex < 25;// норма веса
+            bool isAboveNormal = bodyMassIndex >= 25 && bodyMassIndex <= 30;
+            bool isToolFat = bodyMassIndex > 30;
+
+            bool isFat = isAboveNormal || isToolFat;
+
+            if (isFat)
+            {
+                Console.WriteLine("Жиропасина!");
+            }
+            else
+            {
+                Console.WriteLine("Пока еще не жиропасина!");
+            }
+            if (!isFat)
+            {
+                Console.WriteLine("Ты не жиропасина!");
+            }
+
+            if (isTooLow)
+            {
+                Console.WriteLine("Ты анерексия ходячая!");
+            }
+            else if (isNormal)
+            {
+                Console.WriteLine("Ты идеален!!!");
+            }
+            else if (isAboveNormal)
+            {
+                Console.WriteLine("Обрастаешь гавножиром!!!");
+            }
+            else
+            {
+                Console.WriteLine("Пора не жрать!!!");
+            }
+
+            if (isFat || isToolFat)
+            {
+                Console.WriteLine("Время сесть на диету!!!");
+            }
+
+            string desc = age > 18 ? "Опять нажрешся скотиняка!!!" : "Тебе еще рано побухивать!!!";//ternay operation 
+            Console.WriteLine(desc);
+
+
+            Console.WriteLine("Введите число");
+            int num_a = int.Parse(Console.ReadLine());
+            int num_b = int.Parse(Console.ReadLine());
+            string result = num_a > num_b ? $"{num_a} больше {num_b}" : $"{num_b} больше {num_a}";
+
+            Console.WriteLine(result);
+        }
+    }
+}
